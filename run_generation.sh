8be6a76da4b2f450c7b3a35affdd084ab4ba7ed9
Xingjian Shi <xshiab@connect.ust.hk>
2021-07-05 18:19:31 -0700

    Update run_generation.sh

--- run_generation.sh
+++ run_generation.sh
@@ -6,8 +6,8 @@ python3 generation_example.py \
     --output_dir ./ \
     --input "Why AutoGluon is great?" \
     --max_length 800 \
-    --top_p 0.9 \
+    --top_p 1.0 \
     --top_k 0 \
     --download_dir ./ \
-    --seed 123 \
-    --dtype float16 | tee sample.txt
+    --seed 234 \
+    --dtype bfloat16 | tee sample.txt
